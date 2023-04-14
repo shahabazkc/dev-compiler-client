eb6e7f12150a41ba591721b7c935248feb3d00c9
Shahabaz Kc <shahabaz@embifi.in>
2023-04-14 22:59:39 +0530

    refactor: structuring modules;

---
+++ pages/_app.tsx
@@ -0,0 +1,18 @@
+
+import type { ReactElement, ReactNode } from 'react'
+import type { NextPage } from 'next'
+import type { AppProps } from 'next/app'
+import Layout from '@/components/layout'
+import '../src/styles/globals.css';
+
+export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
+    getLayout?: (page: ReactElement) => ReactNode
+}
+
+type AppPropsWithLayout = AppProps & {
+    Component: NextPageWithLayout
+}
+
+export default function MyApp({ Component, pageProps }: AppPropsWithLayout) {
+    return <Layout><Component {...pageProps} /></Layout>
+}
