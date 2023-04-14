55cf1351b373c3ad9366ca79b75c882ad73cbbf3
Shahabaz Kc <shahabaz@embifi.in>
2023-04-14 23:58:41 +0530

    refactor: types added;

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,17 +1,9 @@
 
-import type { ReactElement, ReactNode } from 'react'
-import type { NextPage } from 'next'
-import type { AppProps } from 'next/app'
+import React from 'react'
 import Layout from '@/components/layout'
 import '../src/styles/globals.css';
+import { AppPropsWithLayout } from '@/types/NextPageWithLayout';
 
-export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
-    getLayout?: (page: ReactElement) => ReactNode
-}
-
-type AppPropsWithLayout = AppProps & {
-    Component: NextPageWithLayout
-}
 
 export default function MyApp({ Component, pageProps }: AppPropsWithLayout) {
     return <Layout><Component {...pageProps} /></Layout>
