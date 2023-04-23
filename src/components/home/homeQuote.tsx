098696257dcf09a0b9687b4d0c2b2babd0e69ff8
Shahabaz Kc <shahabaz@embifi.in>
2023-04-24 02:02:43 +0530

    fix: eslint

--- src/components/home/homeQuote.tsx
+++ src/components/home/homeQuote.tsx
@@ -12,7 +12,7 @@ export const HomeQuote = () => {
                 </div>
                 <div className={`${styles.homeQuoteText}`}>
                     <p>
-                        "The best way to predict the future is to invent it." - Alan Kay Online coding environments are allowing developers to invent new ways of working and collaborating, paving the way for a more innovative and efficient future in software development.
+                    &quot;The best way to predict the future is to invent it.&quot; - Alan Kay Online coding environments are allowing developers to invent new ways of working and collaborating, paving the way for a more innovative and efficient future in software development.
                     </p>
                 </div>
             </div>
